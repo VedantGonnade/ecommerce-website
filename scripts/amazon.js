c330c403209f476d1a36aaffadd359e5ce2b928c
Vedant Gonnade <vedantgonnade@outlook.com>
2023-12-12 20:35:59 +0100

    cart quantity will take the value selected from dropdown

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -61,21 +61,24 @@ document.querySelectorAll(".js-add-to-cart")
     button.addEventListener("click", () => {
       const productId = button.dataset.productId;
       let matchingItem;
+      let cartQuantity = 0;
       cart.forEach((item) => {
         if (item.productId === productId) {
           matchingItem = item;
         }
       });
+
+      const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
         
-      if (matchingItem) matchingItem.quantity++;
+      if (matchingItem) matchingItem.quantity += quantity;
       else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity,
         })
       }
 
-      let cartQuantity = 0;
+     
       cart.forEach((item) => {
           cartQuantity += item.quantity
       })
